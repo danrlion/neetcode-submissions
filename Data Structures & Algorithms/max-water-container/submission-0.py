a581f866a51ce4df2a0d355ab23ac5244802d340
danrlion <61756631+danrlion@users.noreply.github.com>
2026-07-25 17:16:35 +0200

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        # maximize distance between indexes of bars and heights
+        # sum distances between indexes + min(heights)
+        # combination of bars: 1+2, 1+3, 1+4, ..., 3+4, 3+5, etc
+        max_water = 0
+        for side_A_ind in range(0, len(heights)-1):
+            for side_B_ind in range(1, len(heights)):
+                if side_A_ind < side_B_ind:
+                    area_container = (min(heights[side_A_ind], heights[side_B_ind]) * 
+                        (side_B_ind - side_A_ind)
+                    )
+                    if area_container > max_water:
+                        max_water = area_container
+        return max_water
