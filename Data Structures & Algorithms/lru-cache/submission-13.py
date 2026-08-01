fd098677b0c9705c1ff3a58167d9ed4271b46a87
danrlion <61756631+danrlion@users.noreply.github.com>
2026-08-01 12:52:12 +0200

    Add: lru-cache - submission-13

---
+++ Data Structures & Algorithms/lru-cache/submission-13.py
@@ -0,0 +1,30 @@
+from typing import Dict, List
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache: Dict[int, int] = {}
+        self.order_used: List[int] = []
+
+    def update_lru(self, key: int):
+        if key not in self.order_used:
+            self.order_used.append(key)
+        else:
+            ind = self.order_used.index(key)
+            # new version line: improves runtime
+            self.order_used = self.order_used[:ind] + self.order_used[ind+1:] + [key]
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.update_lru(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) == self.capacity and key not in self.cache:
+            key_least_used = self.order_used[0]
+            del self.cache[key_least_used]
+            self.order_used = self.order_used[1:]
+        self.update_lru(key)
+        self.cache[key] = value
